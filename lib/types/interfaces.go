aa32cf6801b33c443fe508c8aa45d0d7ece28d4d
Thomas Pyle <thomas@pyle.tech>
2024-01-22 22:22:09 -0500

    Some work towards setting up Docker Compose Setup

--- lib/types/interfaces.go
+++ lib/types/interfaces.go
@@ -1,21 +1,23 @@
 package types
 
 import (
+	"io"
+
 	"github.com/tpyle/testify/lib/types/result"
 )
 
 type Setup interface {
 	Validate() error
-	Setup() (interface{}, error)
+	Setup(context map[string]string, logFile *io.Writer) (interface{}, error)
 	Teardown() error
 }
 
 type ReadyCheck interface {
 	Validate() error
-	WaitForReady(context interface{}) error
+	WaitForReady(context interface{}, logFile *io.Writer) error
 }
 
 type Runner interface {
 	Validate() error
-	Run(context interface{}) (result.ResultGroup, error)
+	Run(context interface{}, logFile *io.Writer) (result.ResultGroup, error)
 }
--- lib/types/setup/docker_compose.go
+++ lib/types/setup/docker_compose.go
@@ -2,16 +2,31 @@ package setup
 
 import (
 	"fmt"
+	"io"
 	"os"
 	"os/exec"
 
 	"github.com/google/uuid"
 )
 
+type DockerComposeContainerContext struct {
+	Networks       []string          `json:"networks"`
+	ID             string            `json:"id"`
+	Image          string            `json:"image"`
+	Name           string            `json:"name"`
+	PublishedPorts map[string]string `json:"published_ports"`
+}
+
+type DockerComposeContext struct {
+	Networks []string `json:"networks"`
+}
+
 type DockerComposeSetup struct {
-	projectName string   `json:"-"`
-	Files       []string `json:"files"`
-	Services    []string `json:"services"`
+	projectName string            `json:"-"`
+	Files       []string          `json:"files"`
+	Services    []string          `json:"services"`
+	EnvFile     string            `json:"env_file"`
+	ExtraEnv    map[string]string `json:"extra_env"`
 }
 
 func (d *DockerComposeSetup) GetProjectName() string {
@@ -41,10 +56,53 @@ func (d *DockerComposeSetup) Validate() error {
 		}
 	}
 
+	if _, err := os.Stat(d.EnvFile); err != nil {
+		return fmt.Errorf("env file %s not found: %w", d.EnvFile, err)
+	}
+
 	return nil
 }
 
-func (d *DockerComposeSetup) Setup() (interface{}, error) {
+func (d *DockerComposeSetup) generateBaseCommand() []string {
+	args_slice := []string{"docker", "compose", "--project-name", d.GetProjectName()}
+	for _, v := range d.Files {
+		args_slice = append(args_slice, "--file", v)
+	}
+	if d.EnvFile != "" {
+		args_slice = append(args_slice, "--env-file", d.EnvFile)
+	}
+	return args_slice
+}
+
+func (d *DockerComposeSetup) generateUpCommand() []string {
+	args_slice := d.generateBaseCommand()
+	args_slice = append(args_slice, "up", "-d")
+	for _, v := range d.Services {
+		args_slice = append(args_slice, v)
+	}
+	return args_slice
+}
+
+func (d *DockerComposeSetup) generateDownCommand() []string {
+	args_slice := d.generateBaseCommand()
+	args_slice = append(args_slice, "down")
+	return args_slice
+}
+
+func (d *DockerComposeSetup) Setup(context map[string]string, logFile *io.Writer) (interface{}, error) {
+	args_slice := d.generateUpCommand()
+	cmd := exec.Command(args_slice[0], args_slice[1:]...)
+
+	cmd.Env = os.Environ()
+	for k, v := range d.ExtraEnv {
+		cmd.Env = append(cmd.Env, fmt.Sprintf("%s=%s", k, v))
+	}
+	cmd.Stdout = *logFile
+	cmd.Stderr = *logFile
+
+	if err := cmd.Run(); err != nil {
+		return nil, fmt.Errorf("error running docker compose: %w", err)
+	}
 	return nil, nil
 }
 
